6c084da5a2a98a2ccfee35841ff62e1ae4ce74a2
Ibrahim <heema2019n@gmail.com>
2022-09-05 08:10:46 +0300

    upload

---
+++ sc.js
@@ -0,0 +1,41 @@
+var closebtn = document.querySelector('.fa-circle-xmark')
+var menubtn = document.querySelector('.fa-bars');
+var navbar = document.querySelector('.navbar');
+var ancor = document.querySelector('.an');
+var ancor2 = document.querySelector('.an2');
+var ancor3 = document.querySelector('.an3');
+var ancor4 = document.querySelector('.an4');
+var ancor5 = document.querySelector('.an5');
+
+
+
+menubtn.onclick = function () {
+    navbar.style.transform = ' translateX(0%)';
+}
+
+closebtn.onclick = function () {
+    navbar.style.transform = ' translateX(-100%)'
+}
+
+ancor.onclick = function () {
+        navbar.style.transform = ' translateX(-100%)'
+
+}
+ancor2.onclick = function () {
+        navbar.style.transform = ' translateX(-100%)'
+
+}
+
+ancor3.onclick = function () {
+        navbar.style.transform = ' translateX(-100%)'
+
+}
+
+ancor4.onclick = function () {
+        navbar.style.transform = ' translateX(-100%)'
+
+}
+ancor5.onclick = function () {
+        navbar.style.transform = ' translateX(-100%)'
+
+}
